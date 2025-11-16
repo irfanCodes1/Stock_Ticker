737860a7fa38d22b5ae9bd84be3a01dc05244d46
Irfan Ali <irfankk85231@gmail.com>
2025-11-17 01:23:51 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+import { getStockData } from './fakeStockApi.js';
+
+setInterval(function(){
+    const stockData=getStockData();
+    renderStockTicker(stockData);
+},1500)
+
+
+
+function renderStockTicker(stockData){
+    const stockDisplayName=document.getElementById("name");
+    const stockDisplaySymbol=document.getElementById("symbol");
+    const stockDisplayPrice=document.getElementById("price");
+    const priceIcon=document.getElementById("price-icon")
+    const stockDisplayTime=document.getElementById("time");
+
+    const  {name,symbol,price,time}=stockData;
+
+    stockDisplayName.innerText=`Name:   ${name}`;
+    stockDisplaySymbol.innerText=`Symbol:  ${symbol}`
+    stockDisplayPrice.innerText=`Price:  ${price}`;
+    stockDisplayTime.innerText=`Time:  ${time}`;
+}
