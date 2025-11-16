9a079c1edd3dfc9c7b377f410a311d2b9fd2e25b
Irfan Ali <irfankk85231@gmail.com>
2025-11-17 01:26:50 +0500

    Update fakeStockApi.js

--- fakeStockApi.js
+++ fakeStockApi.js
@@ -1,7 +1,7 @@
 export function getStockData(){
     return {
-        name:'closeAi',
-        symbol:'CLA',
+        name:'QtechAI',
+        symbol:'QTA',
         price: (Math.random()*3).toFixed(2),
         time: new Date().toLocaleTimeString()
     }
